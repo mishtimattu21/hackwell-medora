038d66ad690533d0794d41b9279becea6126f69b
mishtimattu21 <mishtimattu21@gmail.com>
2025-09-09 14:56:14 +0530

    header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Menu, X } from "lucide-react";
-import { Menu, X } from "lucide-react";
 import { ThemeToggle } from "./ThemeToggle";
 
 const Header = () => {
